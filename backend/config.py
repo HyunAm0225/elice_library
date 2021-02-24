b99a7a993eafc5ee0312145363f9a31683472f32
HyunAm <tlfgjawlq@naver.com>
2021-02-24 11:27:12 +0900

    1.1 db mysql로변경

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,16 @@
 import os
+import json
+import pymysql
+
 
 BASE_DIR = os.path.dirname(__file__)
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'main.db'))
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+db = {
+    'user': 'elice',
+    'password': 'admin1234',
+    'host': 'elice-kdt-ai-track-vm-racer-49.koreacentral.cloudapp.azure.com',
+    'port': '3306',
+    'database': 'main'
+}
+
+SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnector://{db['user']}:{db['password']}@{db['host']}:{db['port']}/{db['database']}?charset=utf8"
--- backend/migrations/versions/90e784fe9d52_.py
+++ backend/migrations/versions/90e784fe9d52_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: fa4a73190328
+Revision ID: 90e784fe9d52
 Revises: 
-Create Date: 2021-02-23 17:15:04.680297
+Create Date: 2021-02-24 00:52:14.092648
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'fa4a73190328'
+revision = '90e784fe9d52'
 down_revision = None
 branch_labels = None
 depends_on = None

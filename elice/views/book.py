b7b95b49f792fbd2647c6e2ba97fc1d675dea089
Am Hyun <rkgnsp03@gmail.com>
2021-02-25 16:57:07 +0900

    1.2 book detail 페이지 구현 및 book data 추가

--- elice/views/book.py
+++ elice/views/book.py
@@ -1,9 +1,18 @@
 from flask import Blueprint
 from flask import request, render_template, session, url_for, redirect, jsonify
+from elice.models import Book
 
 bp = Blueprint('book', __name__, url_prefix='/book')
 
 
 @bp.route('/')
-def index():
-    return 'book page test'
+def index(methods=["GET"]):
+    if request.method == "GET":
+        books = Book.query.all()
+        return render_template("book/index.html", books=books)
+
+
+@bp.route('/detail/<int:book_id>')
+def detail(book_id):
+    book = Book.query.filter_by(id=book_id).first()
+    return render_template('book/detail.html', book=book)

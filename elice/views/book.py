845ac2e8f9e3e1ddad68b77de89cf45111ab9d38
Am Hyun <rkgnsp03@gmail.com>
2021-03-04 18:04:21 +0900

    2.6 book.py안에 있는 comment 분리

--- elice/views/book.py
+++ elice/views/book.py
@@ -75,16 +75,3 @@ def return_list():
 
     book_rentals = Rental.query.filter_by(user_id=user_id, return_date=None).all()
     return render_template('book/return_list.html', return_book_list=book_rentals)
-
-
-@bp.route('/comment/<int:book_id>', methods=["POST"])
-def comment(book_id):
-    user_id = session['user_id']
-    stars = request.form.get("rating")
-    comment = request.form.get("comment")
-    if comment == None or stars == None:
-        flash("모든 항목을 채워주세요")
-    new_comment = Comment(comment=comment, stars=stars, user_id=user_id, book_id=book_id)
-    db.session.add(new_comment)
-    db.session.commit()
-    return redirect(url_for('book.detail', book_id=book_id))
